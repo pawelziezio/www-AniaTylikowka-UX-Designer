20005fd586c57380ba6ff7eee02d8ae208fa3ac4
pawelziezio <pawel.ziezio@yahoo.com>
2017-11-27 22:50:12 +0100

    parallax + continued

--- myScripts.js
+++ myScripts.js
@@ -7,3 +7,43 @@ navBars.addEventListener('click',function(){
   mainMenu.style.top = top;
   mainMenu.classList.toggle('show');
 })
+
+// --- parallax ----------------
+
+var images = document.querySelectorAll('.js-parallax-bg');
+console.log(images);
+
+function getViewportHeight() {
+    var a = document.documentElement.clientHeight, b = window.innerHeight;
+    return a < b ? b : a;
+}
+
+function getViewportScroll() {
+    if(typeof window.scrollY != 'undefined') {
+        return window.scrollY;
+    }
+    if(typeof pageYOffset != 'undefined') {
+        return pageYOffset;
+    }
+    var doc = document.documentElement;
+    doc = doc.clientHeight ? doc : document.body;
+    return doc.scrollTop;
+}
+
+function doParallax() {
+    var el, elOffset, elHeight,
+        offset = getViewportScroll(),
+        vHeight = getViewportHeight();
+
+    for(var image of images) {
+        el = image;
+        elOffset = el.offsetTop;
+        elHeight = el.offsetHeight;
+
+        if((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) { continue; }
+
+        el.style.backgroundPosition = '50% '+Math.round((elOffset - offset)*3/8)+'px';
+    }
+}
+
+window.addEventListener('scroll', doParallax);
